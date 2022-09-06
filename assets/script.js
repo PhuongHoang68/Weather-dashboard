d525080ddcbd25194d3e7a8ebe8510cb78fb87d8
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-09-05 23:56:49 -0500

    fetch api openweather

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,30 @@
-var getCityData = function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=669456fc6ce6179395a5c719d69a9512");
+var searchFormEl = document.querySelector("#search-form");
+var searchCityEl = document.querySelector("#searchCity");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    //get value from input element
+    var cityInput = searchCityEl.value.trim();
+
+    if(cityInput){
+        getCityInput(cityInput);
+        searchCityEl.value = "";
+    } else {
+        alert("Please enter a City name")
+    }
+
+    console.log(event);
+}
+
+var getCityInput = function(){
+    var apiUrl="https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=669456fc6ce6179395a5c719d69a9512";
+    // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
 };
 
-getCityData();
+searchFormEl.addEventListener("submit", formSubmitHandler);
